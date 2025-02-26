89565c83fd9acc485f71ba9d88352a43f0865dfd
manjunathj026 <165322681+manjunathj026@users.noreply.github.com>
2025-02-26 21:52:10 +0000

    First Network  Commit

---
+++ WebPage/Webserver.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require('http');
+const PORT = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello! This is a sample webpage created using Node.js on Docker!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
